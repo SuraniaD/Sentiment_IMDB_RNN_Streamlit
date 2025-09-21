af38a68f4393a9f0e2286fb5f6b2bc6bc3985ed3
Deepanshu Surania <90096711+SuraniaD@users.noreply.github.com>
2025-09-21 17:28:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Step 1: Import Libraries and Load the Model
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
@@ -47,5 +47,6 @@ if st.button('Classify'):
     st.write(f'Prediction Score: {prediction[0][0]}')
 else:
     st.write('Please enter a movie review.')
+
 
 
